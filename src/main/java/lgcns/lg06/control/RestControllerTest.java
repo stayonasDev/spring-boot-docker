a58a65aab8941ae87f2534934b9893a025327155
stayonasDev <stay_on_track@naver.com>
2025-10-23 23:56:00 +0900

    도커 파일 변경

--- src/main/java/lgcns/lg06/control/RestControllerTest.java
+++ src/main/java/lgcns/lg06/control/RestControllerTest.java
@@ -1,32 +1,21 @@
 package lgcns.lg06.control;
 
-import lgcns.lg06.domain.CurlResponseDTO;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.GetMapping;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Calendar;
-import java.util.TimeZone;
 
 @RestController
-@Log4j2
 public class RestControllerTest {
 
-    @GetMapping("/test")
-    public ResponseEntity<CurlResponseDTO> test() {
-        return ResponseEntity.ok(createWelcomeMessage());
-    }
+        @GetMapping("/hello")
+        public Map<String, Object> sayHello(){
 
-    public CurlResponseDTO createWelcomeMessage(){
-        return CurlResponseDTO.builder()
-                .koreanTime(ZonedDateTime.now(ZoneId.of("Asia/Seoul")) + "[Asia/Seoul]")
-                .timeStamp(System.currentTimeMillis())
-                .message("Hello, World")
-                .build();
-    }
+                return Map.of(
+                                "message", "Hello, World!",
+                                "timesptamp", System.currentTimeMillis(),
+                                "koreatime", ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toString()
+                             );
+        }
 }
